f233d1cbba757cd9e3964e8408c8ee26b57121b8
rudrarana110722-ux <rudrarana110722@gmail.com>
2026-02-09 17:28:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const screens = document.querySelectorAll(".screen");
+const correctBtns = document.querySelectorAll(".correct");
+let current = 0;
 
+correctBtns.forEach(btn=>{
+  btn.addEventListener("click", ()=>{
+    screens[current].classList.add("hidden");
+    current++;
+    screens[current].classList.remove("hidden");
+  });
+});
+
+document.getElementById("yesBtn").addEventListener("click", ()=>{
+  screens[current].classList.add("hidden");
+  document.getElementById("final").classList.remove("hidden");
+});
